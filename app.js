ca8babb76e7730f5d3e6824ca85601a288bb071a
Osayuki <116437863+osayuki1@users.noreply.github.com>
2024-09-15 12:42:23 +0100

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+<script>
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  darkModeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  });
+</script>
